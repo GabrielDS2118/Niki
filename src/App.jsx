2fd2a9dfbf55e0ef773836a1c0d08748d2e521d9
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-08-23 10:39:54 -0500

    4.NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import {
   SuperQuality,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <main className='relative'>
-      Nav
+      <Nav/>
     <section className='xl:padding-l wide:padding-r padding-b'>
      <Hero/>
     </section>
