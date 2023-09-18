1db3124360fb79831540bbd292d42d486dfbe94c
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-09-18 18:30:11 -0500

    13.Terminando footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons";
 import {footerLogo} from '../assets/images'
 import {socialMedia,footerLinks} from '../constants'
 const Footer = () => {
@@ -27,7 +28,9 @@ const Footer = () => {
         //  style={{'border': '1px solid orange'}}
         >
           {socialMedia.map((icon) => (
-            <div className='flex justify-center items-center w-12 h-12 bg-white rounded-full'>
+            <div className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
+            key={icon.alt}
+            >
               <img
                src={icon.src}
                alt={icon.alt}
@@ -46,7 +49,9 @@ const Footer = () => {
               <h4 className='text-white font-montserrat text-2xl leading-normal font-medium mb-6'>{section.title}</h4>
               <ul>
                 {section.links.map((link) => (
-                  <li className='mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer'>
+                  <li className='mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer'
+                  key={link.name}
+                  >
                     <a>{link.name}</a>
                   </li>
                 ))}
@@ -56,6 +61,20 @@ const Footer = () => {
         </div>
 
         
+      </div>
+
+      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center'>
+        <div className='flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer'>
+          <img
+            src={copyrightSign}
+            alt='copyright sign'
+            width={20}
+            height={20}
+            className='rounded-full m-0'
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
       </div>
 
     </footer>
